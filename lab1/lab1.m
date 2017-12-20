1b330e527b39ec97ee8d1c1c65081936067076c4
damaro05 <scmh05@gmail.com>
2017-12-20 10:28:13 +0100

    Metric rectification first approach, affine and metric rectification on the left facade image test

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -28,7 +28,9 @@ H = [s.*R, t;
      0, 0, 1];
  
 I2 = apply_H(I, H);
-figure; imshow(I); figure; imshow(uint8(I2));
+figure(1);
+subplot(1,2,1); imshow(I);         title('Original Image');
+subplot(1,2,2); imshow(uint8(I2)); title('Similarity transformation');
 
 %% 1.2. Affinities
 % ToDo: generate a matrix H which produces an affine transformation
@@ -52,7 +54,11 @@ H = [A, t;
      0, 0, 1];
 
 I2 = apply_H(I, H);
-figure; imshow(I); figure; imshow(uint8(I2));
+%figure; imshow(I); figure; imshow(uint8(I2));
+figure(2);
+subplot(1,2,1); imshow(I);         title('Original Image');
+subplot(1,2,2); imshow(uint8(I2)); title('Affine transformation');
+
 %%
 % ToDo: decompose the affinity in four transformations: two
 % rotations, a scale, and a translation
@@ -117,8 +123,10 @@ H = [A, t;
      v', 1];
  
 I2 = apply_H(I, H);
-figure; imshow(I); figure; imshow(uint8(I2));
-
+%figure; imshow(I); figure; imshow(uint8(I2));
+figure(1);
+subplot(1,2,1); imshow(I);         title('Original Image');
+subplot(1,2,2); imshow(uint8(I2)); title('Projective transformation');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 2. Affine Rectification
 
@@ -224,7 +232,7 @@ fprintf('Angles after affine rectification: %.4f and %.4f\n',angle13p,angle24p);
 %       the metric rectification) with the chosen lines printed on it.
 %       Compute also the angles between the pair of lines before and after
 %       rectification.
-
+[I3, H_a] = metricRectification(I2, H, A, 424, 240, 712, 565);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 4. Affine and Metric Rectification of the left facade of image 0001
 
@@ -232,6 +240,97 @@ fprintf('Angles after affine rectification: %.4f and %.4f\n',angle13p,angle24p);
 %       the stratified method (affine + metric). 
 %       Crop the initial image so that only the left facade is visible.
 %       Show the (properly) transformed lines that use in every step.
+% choose the image points
+I = imread('Data/0001_s.png');
+A = load('Data/0001_s_info_lines.txt');
+% indices of lines
+i = 614;
+p1 = [A(i,1) A(i,2) 1]';
+p2 = [A(i,3) A(i,4) 1]';
+i = 159;
+p3 = [A(i,1) A(i,2) 1]';
+p4 = [A(i,3) A(i,4) 1]';
+i = 645;
+p5 = [A(i,1) A(i,2) 1]';
+p6 = [A(i,3) A(i,4) 1]';
+i = 541;
+p7 = [A(i,1) A(i,2) 1]';
+p8 = [A(i,3) A(i,4) 1]';
+
+% ToDo: compute the lines l1, l2, l3, l4, that pass through the different pairs of points
+l1 = cross(p1,p2);
+l1 = l1./l1(3);
+l2 = cross(p3,p4);
+l2 = l2./l2(3);
+l3 = cross(p5,p6);
+l3 = l3./l3(3);
+l4 = cross(p7,p8);
+l4 = l4./l4(3);
+
+% show the chosen lines in the image
+figure;imshow(I);
+hold on;
+t=1:0.1:1000;
+plot(t, -(l1(1)*t + l1(3)) / l1(2), 'y');
+plot(t, -(l2(1)*t + l2(3)) / l2(2), 'y');
+plot(t, -(l3(1)*t + l3(3)) / l3(2), 'y');
+plot(t, -(l4(1)*t + l4(3)) / l4(2), 'y');
+
+% ToDo: compute the homography that affinely rectifies the image
+v1 = cross(l1,l2);
+v2 = cross(l3,l4);
+
+l_inf = cross(v1,v2);
+% l_inf = l_inf./sqrt(l_inf(1)^2+l_inf(2)^2);
+l_inf = l_inf./l_inf(3);
+
+H = [1 0 0; 0 1 0; l_inf(1) l_inf(2) l_inf(3)];
+
+I2 = apply_H(I, H);%inv(H));
+figure; imshow(uint8(I2));
+
+% ToDo: compute the transformed lines lr1, lr2, lr3, lr4
+lr1 = inv(H)'*l1;
+% lr1./sqrt(lr1(1)^2+lr1(2)^2)
+lr1 = lr1./lr1(3);
+
+lr2 = inv(H)'*l2;
+% lr2./sqrt(lr2(1)^2+lr2(2)^2)
+lr2 = lr2./lr2(3);
+
+lr3 = inv(H)'*l3;
+% lr3./sqrt(lr3(1)^2+lr3(2)^2)
+lr3 = lr3./lr3(3);
+
+lr4 = inv(H)'*l4;
+% lr4./sqrt(lr4(1)^2+lr4(2)^2)
+lr4 = lr4./lr4(3);
+
+% show the transformed lines in the transformed image
+figure;imshow(uint8(I2));
+hold on;
+t=1:0.1:1000;
+plot(t, -(lr1(1)*t + lr1(3)) / lr1(2), 'y');
+plot(t, -(lr2(1)*t + lr2(3)) / lr2(2), 'y');
+plot(t, -(lr3(1)*t + lr3(3)) / lr3(2), 'y');
+plot(t, -(lr4(1)*t + lr4(3)) / lr4(2), 'y');
+
+% ToDo: to evaluate the results, compute the angle between the different pair 
+% of lines before and after the image transformation
+omega_inf = eye(3); omega_inf(3,3)=0;
+
+angle13 = acos(dot(omega_inf*l1,l3) / ...
+    ( sqrt(dot(omega_inf*l1,l1)) * sqrt(dot(omega_inf*l3,l3)) ) )*(180/pi);
+angle24 = acos(dot(omega_inf*l2,l4) / ...
+    ( sqrt(dot(omega_inf*l2,l2)) * sqrt(dot(omega_inf*l4,l4)) ) )*(180/pi);
+fprintf('Angles before affine rectification: %.4f and %.4f\n',angle13,angle24);
+angle13p = acos(dot(omega_inf*lr1,lr3) / ...
+    ( sqrt(dot(omega_inf*lr1,lr1)) * sqrt(dot(omega_inf*lr3,lr3)) ) )*(180/pi);
+angle24p = acos(dot(omega_inf*lr2,lr4) / ...
+    ( sqrt(dot(omega_inf*lr2,lr2)) * sqrt(dot(omega_inf*lr4,lr4)) ) )*(180/pi);
+fprintf('Angles after affine rectification: %.4f and %.4f\n',angle13p,angle24p);
+
+[I3, H_a] = metricRectification(I2, H, A, 614, 159, 645, 541);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 5. OPTIONAL: Metric Rectification in a single step
